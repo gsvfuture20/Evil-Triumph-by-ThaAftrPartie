6fd3ac25df8eeaf644c5fa2434e0f95ab10834dd
gsvfuture20 <56543836+gsvfuture20@users.noreply.github.com>
2019-11-20 10:37:26 -0600

    Update Reference.java

--- Evil Triumph/src/main/java/com/ThaAftrPartie/EvilTriumph/util/Reference.java
+++ Evil Triumph/src/main/java/com/ThaAftrPartie/EvilTriumph/util/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String NAME = "Evil Triumph";
 	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-	public static final String CLIENT_PROXY_CLASS = "com.ThaAftrPartie.EvilTriumph.proxy.CommonProxy";
+	public static final String CLIENT_PROXY_CLASS = "com.ThaAftrPartie.EvilTriumph.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.ThaAftrPartie.EvilTriumph.proxy.CommonProxy";
 
 }
