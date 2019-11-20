e28dd9c4f8a415b207ceadb77339adf6b1cd9a1c
gsvfuture20 <56543836+gsvfuture20@users.noreply.github.com>
2019-11-20 10:36:12 -0600

    .

--- Evil Triumph/src/main/java/com/ThaAftrPartie/EvilTriumph/objects/blocks/BlockBase.java
+++ Evil Triumph/src/main/java/com/ThaAftrPartie/EvilTriumph/objects/blocks/BlockBase.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.ThaAftrPartie.EvilTriumph.Main;
 import com.ThaAftrPartie.EvilTriumph.init.ModBlocks;
 import com.ThaAftrPartie.EvilTriumph.init.ModItems;
-import com.ThaAftrPartie.EvilTriumph.util.interfaces.IHasModel;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -30,9 +29,17 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
-public class BlockBase extends Block implements IHasModel
-
+public class BlockBase extends Block
 {
+
+public void registerModels() 
+{
+    Main.proxy.registerItemRednerer(this, 0, "inventory");
+    
+    
+}
+
+
 
 	public BlockBase(String name, Material material)
 	{
@@ -47,163 +54,5 @@ public class BlockBase extends Block implements IHasModel
 	ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 	
 	
-}
-
-	@Override
-	public void registerModels() {
-		
-		Main.proxy.registerItemRednerer(Item.getItemFromBlock(this), 0, "inventory" );
-		
-	}
-
-
-
-@Override
-public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean isActualState) 
-{
-	
-}
-
-@Override
-public boolean addDestroyEffects(World world, BlockPos pos, ParticleManager manager) 
-{
-	return super.addDestroyEffects(world, pos, manager);
-}
-
-@Override
-public boolean addHitEffects(IBlockState state, World worldObj, RayTraceResult target, ParticleManager manager) 
-{
-	return super.addHitEffects(state, worldObj, target, manager);
-}
-
-public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced) 
-{
-	
-}
-
-
-@Override
-public boolean addLandingEffects(IBlockState state, WorldServer worldObj, BlockPos blockPosition, IBlockState iblockstate, EntityLivingBase entity, int numberOfParticles) 
-{
-	return super.addLandingEffects(state, worldObj, blockPosition, iblockstate, entity, numberOfParticles);
-}
-
-
-@Override
-public boolean addRunningEffects(IBlockState state, World world, BlockPos pos, Entity entity) 
-{
-	return super.addRunningEffects(state, world, pos, entity);
-}
-
-@Override
-public void beginLeavesDecay(IBlockState state, World world, BlockPos pos) 
-{
-	
-}
-
-@Override
-public void breakBlock(World worldIn, BlockPos pos, IBlockState state) 
-{
-	
-}
-
-@Override
-public boolean canBeConnectedTo(IBlockAccess world, BlockPos pos, EnumFacing facing) 
-{
-	return super.canBeConnectedTo(world, pos, facing);
-}
-
-@Override
-public boolean canBeReplacedByLeaves(IBlockState state, IBlockAccess world, BlockPos pos) 
-{
-	return super.canBeReplacedByLeaves(state, world, pos);
-}
-
-@Override
-public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid) 
-{
-	return super.canCollideCheck(state, hitIfLiquid);
-}
-
-@Override
-public boolean canConnectRedstone(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) 
-{
-	return super.canConnectRedstone(state, world, pos, side);
-}
-
-@Override
-public boolean canCreatureSpawn(IBlockState state, IBlockAccess world, BlockPos pos, SpawnPlacementType type) 
-{
-	return super.canCreatureSpawn(state, world, pos, type);
-}
-
-@Override
-public boolean canDropFromExplosion(Explosion explosionIn) 
-{
-	return super.canDropFromExplosion(explosionIn);
-}
-
-@Override
-public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) 
-{
-	return super.canEntityDestroy(state, world, pos, entity);
-}
-
-
-@Override
-@SuppressWarnings("deprecation")
-public boolean canEntitySpawn(IBlockState state, Entity entityIn) 
-{
-	return super.canEntitySpawn(state, entityIn);
-}
-
-@Override
-public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player) 
-{
-	return super.canHarvestBlock(world, pos, player);
-}
-
-@Override
-public boolean canPlaceBlockAt(World worldIn, BlockPos pos) 
-{
-	return super.canPlaceBlockAt(worldIn, pos);
-}
-
-@Override
-public boolean canPlaceBlockOnSide(World worldIn, BlockPos pos, EnumFacing side) 
-{
-	return super.canPlaceBlockOnSide(worldIn, pos, side);
-}
-
-@Override
-public boolean canPlaceTorchOnTop(IBlockState state, IBlockAccess world, BlockPos pos) 
-{
-	return super.canPlaceTorchOnTop(state, world, pos);
-}
-
-@SuppressWarnings("deprecation")
-@Override
-public boolean canProvidePower(IBlockState state)
-{
-	return super.canProvidePower(state);
-}
-
-@Override
-public boolean canRenderInLayer(IBlockState state, BlockRenderLayer layer) 
-{
-	return super.canRenderInLayer(state, layer);
-}
-
-@SuppressWarnings("deprecation")
-@Override
-protected boolean canSilkHarvest() 
-{
-	return super.canSilkHarvest();
-}
-
-@Override
-public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) 
-{
-	return super.canSilkHarvest(world, pos, state, player);
 }
 }
--- Evil Triumph/src/main/java/com/ThaAftrPartie/EvilTriumph/objects/items/ItemBase.java
+++ Evil Triumph/src/main/java/com/ThaAftrPartie/EvilTriumph/objects/items/ItemBase.java
@@ -1,53 +1,21 @@
 package com.ThaAftrPartie.EvilTriumph.objects.items;
 
-import java.util.List;
-import java.util.Set;
-
 import com.ThaAftrPartie.EvilTriumph.Main;
 import com.ThaAftrPartie.EvilTriumph.init.ModItems;
 import com.ThaAftrPartie.EvilTriumph.tabs.evilTriumph;
-import com.ThaAftrPartie.EvilTriumph.util.interfaces.IHasModel;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Multimap;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.passive.HorseArmorType;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.EnumRarity;
-import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.minecraftforge.common.animation.ITimeValue;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 
-public class ItemBase extends Item implements IHasModel
+public class ItemBase extends Item{
+
+public void registerModels() 
 {
+    Main.proxy.registerItemRednerer(this, 0, "inventory");
+    
+    
+}
+
 	
 
 	
@@ -62,14 +30,5 @@ public class ItemBase extends Item implements IHasModel
 		
 		
 	}
-
-
-
-@Override
-public void registerModels() 
-{
-	Main.proxy.registerItemRednerer(this, 0, "inventory");
-	
-	
 }
-}
+
